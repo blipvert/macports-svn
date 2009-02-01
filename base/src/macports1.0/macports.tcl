blipvert/macports-svn

commit 5266962f3269d7284928e98c6054425c0910de77

mportdepends: change accDeps from a list to an array, and skip out earlier when a dep is satsified.
git-svn-id: https://svn.macports.org/repository/macports/trunk@46058 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/macports1.0/macports.tcl
@@ -1706,11 +1706,16 @@ proc _mportkey {mport key} {
 #                  dependencies ports.
 # accDeps -> accumulator for recursive calls
 # return 0 if everything was ok, an non zero integer otherwise.
-proc mportdepends {mport {target ""} {recurseDeps 1} {skipSatisfied 1} {accDeps {}}} {
+proc mportdepends {mport {target ""} {recurseDeps 1} {skipSatisfied 1} {accDepsFlag 0}} {
 
     array set portinfo [mportinfo $mport]
     set depends {}
     set deptypes {}
+    if {$accDepsFlag == 0} {
+        array set accDeps {}
+    } else {
+        upvar accDeps accDeps
+    }
         
     # Determine deptypes to look for based on target
     switch $target {
@@ -1745,6 +1750,10 @@ proc mportdepends {mport {target ""} {recurseDeps 1} {skipSatisfied 1} {accDeps 
     foreach depspec $depends {
         # grab the portname portion of the depspec
         set dep_portname [lindex [split $depspec :] end]
+        # Skip the port if it's already in the accumulated list.
+        if {$skipSatisfied && [info exists accDeps($dep_portname)]} {
+            continue
+        }
         
         # Find the porturl
         if {[catch {set res [mportsearch $dep_portname false exact]} error]} {
@@ -1781,14 +1790,11 @@ proc mportdepends {mport {target ""} {recurseDeps 1} {skipSatisfied 1} {accDeps 
             ditem_append_unique $mport requires "[ditem_key $subport provides]"
     
             if {$recurseDeps} {
-                # Skip the port if it's already in the accumulated list.
-                if {[lsearch $accDeps $dep_portname] == -1} {
-                    # Add it to the list
-                    lappend accDeps $dep_portname
-                
-                    # We'll recursively iterate on it.
-                    lappend subPorts $subport
-                }
+                # Add it to the list
+                set accDeps($dep_portname) 1
+
+                # We'll recursively iterate on it.
+                lappend subPorts $subport
             }
         }
     }
@@ -1797,7 +1803,7 @@ proc mportdepends {mport {target ""} {recurseDeps 1} {skipSatisfied 1} {accDeps 
     if {$recurseDeps} {
         foreach subport $subPorts {
             # Sub ports should be installed (all dependencies must be satisfied).
-            set res [mportdepends $subport "" $recurseDeps $skipSatisfied $accDeps]
+            set res [mportdepends $subport "" $recurseDeps $skipSatisfied 1]
             if {$res != 0} {
                 return $res
             }

commit 4f00d3579325d519ba7fea56fb9b6667edd84c63

Fix mportdepends breakage introduced in r46058.
git-svn-id: https://svn.macports.org/repository/macports/trunk@46224 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/macports1.0/macports.tcl
@@ -1750,10 +1750,6 @@ proc mportdepends {mport {target ""} {recurseDeps 1} {skipSatisfied 1} {accDepsF
     foreach depspec $depends {
         # grab the portname portion of the depspec
         set dep_portname [lindex [split $depspec :] end]
-        # Skip the port if it's already in the accumulated list.
-        if {$skipSatisfied && [info exists accDeps($dep_portname)]} {
-            continue
-        }
         
         # Find the porturl
         if {[catch {set res [mportsearch $dep_portname false exact]} error]} {
@@ -1790,11 +1786,14 @@ proc mportdepends {mport {target ""} {recurseDeps 1} {skipSatisfied 1} {accDepsF
             ditem_append_unique $mport requires "[ditem_key $subport provides]"
     
             if {$recurseDeps} {
-                # Add it to the list
-                set accDeps($dep_portname) 1
+                # Skip the port if it's already in the accumulated list.
+                if {![info exists accDeps($dep_portname)]} {
+                    # Add it to the list
+                    set accDeps($dep_portname) 1
 
-                # We'll recursively iterate on it.
-                lappend subPorts $subport
+                    # We'll recursively iterate on it.
+                    lappend subPorts $subport
+                }
             }
         }
     }