blipvert/macports-svn

commit 3f9e18c6a7c5261ab02690ff1e18e6dba8dd781c

macports1.0, pextlib1.0: Make sure we always get the right error description by calling Tcl_SetErrno before using Tcl_PosixError as the man page suggest. Also include some additional information in the error messages (e.g. path names).
git-svn-id: https://svn.macports.org/repository/macports/trunk@63799 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/macports1.0/get_systemconfiguration_proxies.c
@@ -91,6 +91,7 @@ int GetSystemConfigurationProxiesCmd( ClientData clientData UNUSED, Tcl_Interp *
         CFRelease( proxies );
     }
     if( cmdResult == TCL_ERROR )
+        Tcl_SetErrno( errno );
         Tcl_SetResult( interp, (char *) Tcl_PosixError( interp ), TCL_STATIC );
 #endif
    return cmdResult;

base/src/pextlib1.0/Pextlib.c
@@ -338,7 +338,9 @@ int CreateSymlinkCmd(ClientData clientData UNUSED, Tcl_Interp *interp, int objc,
     target = Tcl_GetString(objv[2]);
 
     if (symlink(value, target) != 0) {
-        Tcl_SetResult(interp, (char *)Tcl_PosixError(interp), TCL_STATIC);
+        Tcl_SetErrno(errno);
+        Tcl_ResetResult(interp);
+        Tcl_AppendResult(interp, "symlink: ", target, " -> ", value, ": ", (char *)Tcl_PosixError(interp), NULL);
         return TCL_ERROR;
     }
     return TCL_OK;
@@ -450,7 +452,9 @@ int lchownCmd(ClientData clientData UNUSED, Tcl_Interp *interp, int objc, Tcl_Ob
         }
     }
     if (lchown(path, (uid_t) user, (gid_t) group) != 0) {
-        Tcl_SetResult(interp, (char *)Tcl_PosixError(interp), TCL_STATIC);
+        Tcl_SetErrno(errno);
+        Tcl_ResetResult(interp);
+        Tcl_AppendResult(interp, "lchown: ", path, ": ", (char *)Tcl_PosixError(interp), NULL);
         return TCL_ERROR;
     }
 

base/src/pextlib1.0/fs-traverse.c
@@ -201,6 +201,7 @@ do_traverse(Tcl_Interp *interp, int flags, char * CONST *targets, Tcl_Obj *varna
     }
     /* check errno before calling fts_close in case it sets errno to 0 on success */
     if (errno != 0 || (fts_close(root_fts) != 0 && !(flags & F_IGNORE_ERRORS))) {
+        Tcl_SetErrno(errno);
         Tcl_SetResult(interp, (char *)Tcl_PosixError(interp), TCL_STATIC);
         return TCL_ERROR;
     }

commit 0a91b53491e7902c6d05ab1001d240647dcfa101

macports1.0: Fix error introduced in r63799 by adding missing braces.
git-svn-id: https://svn.macports.org/repository/macports/trunk@63800 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/macports1.0/get_systemconfiguration_proxies.c
@@ -90,9 +90,10 @@ int GetSystemConfigurationProxiesCmd( ClientData clientData UNUSED, Tcl_Interp *
             cmdResult = TCL_ERROR;
         CFRelease( proxies );
     }
-    if( cmdResult == TCL_ERROR )
+    if( cmdResult == TCL_ERROR ) {
         Tcl_SetErrno( errno );
         Tcl_SetResult( interp, (char *) Tcl_PosixError( interp ), TCL_STATIC );
+    }
 #endif
    return cmdResult;
 }