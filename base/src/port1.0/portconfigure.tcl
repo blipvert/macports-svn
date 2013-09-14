blipvert/macports-svn

commit d8fb64320749280c4d2a02772d323966138f0366

portconfigure.tcl: Make Autotools verbose by default.
git-svn-id: https://svn.macports.org/repository/macports/trunk@111023 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/port1.0/portconfigure.tcl
@@ -51,9 +51,11 @@ default configure.cmd       ./configure
 default configure.nice      {${buildnicevalue}}
 default configure.dir       {${worksrcpath}}
 default autoreconf.dir      {${worksrcpath}}
-default autoreconf.pre_args {--install}
+default autoreconf.pre_args {--install --verbose}
 default autoconf.dir        {${worksrcpath}}
+default autoconf.pre_args   {--verbose}
 default automake.dir        {${worksrcpath}}
+default automake.pre_args   {--verbose}
 default xmkmf.cmd           xmkmf
 default xmkmf.dir           {${worksrcpath}}
 default use_configure       yes

commit 1ced36a1042c3dd1a98b33970bcdbb6e54aef42c

Fix regression introduced by r111023
git-svn-id: https://svn.macports.org/repository/macports/trunk@111052 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/port1.0/portconfigure.tcl
@@ -51,11 +51,11 @@ default configure.cmd       ./configure
 default configure.nice      {${buildnicevalue}}
 default configure.dir       {${worksrcpath}}
 default autoreconf.dir      {${worksrcpath}}
-default autoreconf.pre_args {--install --verbose}
+default autoreconf.pre_args {-vi}
 default autoconf.dir        {${worksrcpath}}
-default autoconf.pre_args   {--verbose}
+default autoconf.pre_args   {-v}
 default automake.dir        {${worksrcpath}}
-default automake.pre_args   {--verbose}
+default automake.pre_args   {-v}
 default xmkmf.cmd           xmkmf
 default xmkmf.dir           {${worksrcpath}}
 default use_configure       yes