blipvert/macports-svn

commit 987aa5a217777dcc2aa559706b5b8479bd01df1d

muniversal portgroup: use {*} instead of eval where needed, and remove a number of unnecessary evals
git-svn-id: https://svn.macports.org/repository/macports/trunk@134832 d073be05-634f-4543-b044-5fe20cf6d1d6

dports/_resources/port1.0/group/muniversal-1.0.tcl
@@ -124,12 +124,12 @@ variant universal {
         configure.universal_ldflags-delete   -arch ${arch}
     }
 
-    eval configure.args-append      ${configure.universal_args}
-    eval configure.cflags-append    ${configure.universal_cflags}
-    eval configure.cxxflags-append  ${configure.universal_cxxflags}
-    eval configure.objcflags-append ${configure.universal_cflags}
-    eval configure.ldflags-append   ${configure.universal_ldflags}
-    eval configure.cppflags-append  ${configure.universal_cppflags}
+    configure.args-append      {*}${configure.universal_args}
+    configure.cflags-append    {*}${configure.universal_cflags}
+    configure.cxxflags-append  {*}${configure.universal_cxxflags}
+    configure.objcflags-append {*}${configure.universal_cflags}
+    configure.ldflags-append   {*}${configure.universal_ldflags}
+    configure.cppflags-append  {*}${configure.universal_cppflags}
 
     # user has specified that build platform must be able to run binaries for supported architectures
     if { ${merger_must_run_binaries}=="yes" } {
@@ -290,7 +290,7 @@ variant universal {
             set configure_dir_save  ${configure.dir}
             if { [string match "${worksrcpath}/*" ${configure.dir}] } {
                 # The configure directory is inside the source directory, so put in the new source directory name.
-                eval configure.dir  [string map "${worksrcpath} ${worksrcpath}-${arch}" ${configure.dir}]
+                configure.dir [string map "${worksrcpath} ${worksrcpath}-${arch}" ${configure.dir}]
             } else {
                 # The configure directory is outside the source directory, so give it a new name by appending ${arch}.
                 configure.dir  ${configure.dir}-${arch}
@@ -302,7 +302,7 @@ variant universal {
             set autoreconf_dir_save  ${autoreconf.dir}
             if { [string match "${worksrcpath}/*" ${autoreconf.dir}] } {
                 # The autoreconf directory is inside the source directory, so put in the new source directory name.
-                eval autoreconf.dir  [string map "${worksrcpath} ${worksrcpath}-${arch}" ${autoreconf.dir}]
+                autoreconf.dir [string map "${worksrcpath} ${worksrcpath}-${arch}" ${autoreconf.dir}]
             } else {
                 # The autoreconf directory is outside the source directory, so give it a new name by appending ${arch}.
                 autoreconf.dir  ${autoreconf.dir}-${arch}
@@ -314,15 +314,15 @@ variant universal {
             portconfigure::configure_main
 
             # Undo changes to the configure related variables
-            eval autoreconf.dir ${autoreconf_dir_save}
-            eval configure.dir  ${configure_dir_save}
-            eval configure.compiler ${configure_compiler_save}
-            eval configure.f90  ${configure_f90_save}
-            eval configure.f77  ${configure_f77_save}
-            eval configure.fc   ${configure_fc_save}
-            eval configure.cc   ${configure_cc_save}
-            eval configure.cxx  ${configure_cxx_save}
-            eval configure.objc ${configure_objc_save}
+            autoreconf.dir ${autoreconf_dir_save}
+            configure.dir  ${configure_dir_save}
+            configure.compiler ${configure_compiler_save}
+            configure.f90  ${configure_f90_save}
+            configure.f77  ${configure_f77_save}
+            configure.fc   ${configure_fc_save}
+            configure.cc   ${configure_cc_save}
+            configure.cxx  ${configure_cxx_save}
+            configure.objc ${configure_objc_save}
             if { [info exists merger_configure_args(${arch})] } {
                 configure.args-delete  $merger_configure_args(${arch})
             }
@@ -370,7 +370,7 @@ variant universal {
             set build_dir_save  ${build.dir}
             if { [string match "${worksrcpath}/*" ${build.dir}] } {
                 # The build directory is inside the source directory, so put in the new source directory name.
-                eval build.dir  [string map "${worksrcpath} ${worksrcpath}-${arch}" ${build.dir}]
+                build.dir [string map "${worksrcpath} ${worksrcpath}-${arch}" ${build.dir}]
             } else {
                 # The build directory is outside the source directory, so give it a new name by appending ${arch}.
                 build.dir  ${build.dir}-${arch}
@@ -381,7 +381,7 @@ variant universal {
 
             portbuild::build_main
 
-            eval build.dir  ${build_dir_save}
+            build.dir ${build_dir_save}
             if { [info exists merger_build_args(${arch})] } {
                 build.args-delete $merger_build_args(${arch})
             }
@@ -396,7 +396,7 @@ variant universal {
             ui_info "$UI_PREFIX [format [msgcat::mc "Staging %1\$s into destroot for architecture %2\$s"] ${subport} ${arch}]"
             copy ${destroot} ${workpath}/destroot-${arch}
             set destdirSave ${destroot.destdir}
-            eval destroot.destdir  [string map "${destroot} ${workpath}/destroot-${arch}" ${destroot.destdir}]
+            destroot.destdir [string map "${destroot} ${workpath}/destroot-${arch}" ${destroot.destdir}]
 
             if { [info exists merger_destroot_env(${arch})] } {
                 destroot.env-append  $merger_destroot_env(${arch})
@@ -407,7 +407,7 @@ variant universal {
             set destroot_dir_save ${destroot.dir}
             if { [string match "${worksrcpath}/*" ${destroot.dir}] } {
                 # The destroot directory is inside the source directory, so put in the new source directory name.
-                eval destroot.dir  [string map "${worksrcpath} ${worksrcpath}-${arch}" ${destroot.dir}]
+                destroot.dir [string map "${worksrcpath} ${worksrcpath}-${arch}" ${destroot.dir}]
             } else {
                 # The destroot directory is outside the source directory, so give it a new name by appending ${arch}.
                 destroot.dir  ${destroot.dir}-${arch}
@@ -425,7 +425,7 @@ variant universal {
             if { [info exists merger_destroot_env(${arch})] } {
                 destroot.env-delete  $merger_destroot_env(${arch})
             }
-            eval destroot.destdir ${destdirSave}
+            destroot.destdir ${destdirSave}
         }
         delete ${destroot}
 
@@ -719,7 +719,7 @@ variant universal {
                 set test_dir_save ${test.dir}
                 if { [string match "${worksrcpath}/*" ${test.dir}] } {
                     # The test directory is inside the source directory, so put in the new source directory name.
-                    eval test.dir  [string map "${worksrcpath} ${worksrcpath}-${arch}" ${test.dir}]
+                    test.dir [string map "${worksrcpath} ${worksrcpath}-${arch}" ${test.dir}]
                 } else {
                     # The test directory is outside the source directory, so give it a new name by appending ${arch}.
                     test.dir  ${test.dir}-${arch}

commit 417cfd463c240bb3fae214ca7efb5db5538053bf

muniversal-1.0: Correctly restore multi-element options (#47388)
This was a regression caused by r134832.


git-svn-id: https://svn.macports.org/repository/macports/trunk@134863 d073be05-634f-4543-b044-5fe20cf6d1d6

dports/_resources/port1.0/group/muniversal-1.0.tcl
@@ -314,15 +314,15 @@ variant universal {
             portconfigure::configure_main
 
             # Undo changes to the configure related variables
-            autoreconf.dir ${autoreconf_dir_save}
-            configure.dir  ${configure_dir_save}
-            configure.compiler ${configure_compiler_save}
-            configure.f90  ${configure_f90_save}
-            configure.f77  ${configure_f77_save}
-            configure.fc   ${configure_fc_save}
-            configure.cc   ${configure_cc_save}
-            configure.cxx  ${configure_cxx_save}
-            configure.objc ${configure_objc_save}
+            autoreconf.dir      {*}${autoreconf_dir_save}
+            configure.dir       {*}${configure_dir_save}
+            configure.compiler  {*}${configure_compiler_save}
+            configure.f90       {*}${configure_f90_save}
+            configure.f77       {*}${configure_f77_save}
+            configure.fc        {*}${configure_fc_save}
+            configure.cc        {*}${configure_cc_save}
+            configure.cxx       {*}${configure_cxx_save}
+            configure.objc      {*}${configure_objc_save}
             if { [info exists merger_configure_args(${arch})] } {
                 configure.args-delete  $merger_configure_args(${arch})
             }